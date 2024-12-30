f7d13d6b0be2f3244abd6a721f8ada472fc970cd
Anuj12-2004 <anuj.harde_24uds@sanjivani.edu.in>
2024-12-30 16:13:49 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,13 @@
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        // Declare two variables
+        int num1 = 5;
+        int num2 = 10;
+        
+        // Calculate the sum
+        int sum = num1 + num2;
+        
+        // Print the result to the console
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
